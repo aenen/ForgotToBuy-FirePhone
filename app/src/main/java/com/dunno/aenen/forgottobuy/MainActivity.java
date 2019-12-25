47c60b6dd37cd1b289e9922da166bbaeb7b75b2d
aenen <yaroslav.golyuk@gmail.com>
2019-12-25 03:34:54 +0200

    додав кльовий тридешний хедер і суперський прогрес бар на головну актівіті (не знаю навіщо)

--- app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
@@ -1,10 +1,15 @@
 package com.dunno.aenen.forgottobuy;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.amazon.euclid.widget.ZShadowReceiver;
 
 
 public class MainActivity extends Activity {
@@ -14,7 +19,16 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove the title bar, and make the app fullscreen.
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        final ZShadowReceiver headerBackground = (ZShadowReceiver) findViewById(R.id.header_shadow_receiver);
+        headerBackground.setBackgroundColor(Color.MAGENTA);
+
         Log.d("MainActivity", "whi are you");
 
         try {
