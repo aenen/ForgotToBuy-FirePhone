865300b2ad32e2cd49628775ad45af1fa48479b9
aenen <yaroslav.golyuk@gmail.com>
2019-12-25 03:03:04 +0200

    Прохожу уроки по андроїду і намагаюсь використовувати амазонівські штучки : )

--- app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
@@ -2,38 +2,27 @@ package com.dunno.aenen.forgottobuy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Log.d("MainActivity", "whi are you");
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        try {
+            throw new Exception("I'm sorry.....");
         }
-
-        return super.onOptionsItemSelected(item);
+        catch (Throwable ex)
+        {
+            Log.e(LOG_TAG, ex.getMessage());
+        }
     }
 }
