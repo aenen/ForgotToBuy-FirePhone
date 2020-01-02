33b1fa7ebc98bb4cba2d75199e6b017d2d9814ab
aenen <yaroslav.golyuk@gmail.com>
2020-01-02 20:27:11 +0200

    scrollview

--- app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
@@ -7,54 +7,38 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazon.euclid.util.TiltScrollController;
 import com.amazon.euclid.widget.ZShadowReceiver;
 import com.amazon.euclid.widget.ZTextView;
 
 
 public class MainActivity extends Activity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private int mCount = 0;
-    private ZTextView mShowCount;
+    // TiltScrollController used to control tilt scrolling.
+    private TiltScrollController mTiltScrollController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        // Remove the title bar, and make the app fullscreen.
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        try {
+            super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
-        mShowCount = (ZTextView) findViewById(R.id.show_count);
+            // Remove the title bar, and make the app fullscreen.
+            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            setContentView(R.layout.activity_main);
 
-        final ZShadowReceiver headerBackground = (ZShadowReceiver) findViewById(R.id.header_shadow_receiver);
-        headerBackground.setBackgroundColor(Color.GRAY);
-
-        Log.d("MainActivity", "whi are you");
-
-        try {
-            throw new Exception("I'm sorry.....");
-        }
-        catch (Throwable ex)
-        {
-            Log.e(LOG_TAG, ex.getMessage());
-        }
-    }
-
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
-    public void countUp(View view) {
-        ++mCount;
-        if(mShowCount != null)
-            mShowCount.setText(Integer.toString((mCount)));
+            final ZShadowReceiver headerBackground = (ZShadowReceiver) findViewById(R.id.header_shadow_receiver);
+            headerBackground.setBackgroundColor(Color.BLUE);
+//        }
+//        catch(Throwable ex){
+//            Log.e("error", ex.getMessage());
+//        }
     }
 }
