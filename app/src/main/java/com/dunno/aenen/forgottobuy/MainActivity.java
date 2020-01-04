08eb4647d5a7ef9598151731593fcfc8ef9016b6
aenen <yaroslav.golyuk@gmail.com>
2020-01-04 22:33:28 +0200

    передача даних між двома activity

--- app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dunno.aenen.forgottobuy;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,35 +13,66 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amazon.euclid.util.TiltScrollController;
+import com.amazon.euclid.widget.ZContainer;
 import com.amazon.euclid.widget.ZShadowReceiver;
 import com.amazon.euclid.widget.ZTextView;
 
 
 public class MainActivity extends Activity {
 
-    // TiltScrollController used to control tilt scrolling.
-    private TiltScrollController mTiltScrollController;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
+    private EditText mMessageEditText;
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-//        try {
-            super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
 
-            // Remove the title bar, and make the app fullscreen.
-            this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-            setContentView(R.layout.activity_main);
+        // Remove the title bar, and make the app fullscreen.
+//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-            final ZShadowReceiver headerBackground = (ZShadowReceiver) findViewById(R.id.header_shadow_receiver);
-            headerBackground.setBackgroundColor(Color.BLUE);
-//        }
-//        catch(Throwable ex){
-//            Log.e("error", ex.getMessage());
-//        }
+        setContentView(R.layout.activity_main);
+
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
+        mReplyHeadTextView =(TextView) findViewById(R.id.text_header_reply);
+        mReplyTextView =(TextView) findViewById(R.id.text_message_reply);
+
+        final ZContainer headerBackground = (ZContainer) findViewById(R.id.header_shadow_receiver);
+        headerBackground.setBackgroundColor(Color.BLUE);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+
+        Intent intent =  new Intent(this,SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
