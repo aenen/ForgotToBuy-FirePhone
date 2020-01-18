a746cdd48ed52d165bbc47615f55e5807f17a016
aenen <yaroslav.golyuk@gmail.com>
2020-01-18 22:17:12 +0200

    вивчаю лайф сайкл актівіті і як зберігати данні під час зміни статусу

--- app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/MainActivity.java
@@ -39,17 +39,81 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
 
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
+
         // Remove the title bar, and make the app fullscreen.
-//        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
 
+        // Initialize all the view variables.
         mMessageEditText = (EditText) findViewById(R.id.editText_main);
         mReplyHeadTextView =(TextView) findViewById(R.id.text_header_reply);
         mReplyTextView =(TextView) findViewById(R.id.text_message_reply);
 
+        // Restore the state.
+        // See onSaveInstanceState() for what gets saved.
+        if (savedInstanceState != null) {
+            boolean isVisible =
+                    savedInstanceState.getBoolean("reply_visible");
+
+            // Show both the header and the message views. If isVisible is
+            // false or missing from the bundle, use the default layout.
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState.getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+
         final ZContainer headerBackground = (ZContainer) findViewById(R.id.header_shadow_receiver);
         headerBackground.setBackgroundColor(Color.BLUE);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
     }
 
     public void launchSecondActivity(View view) {
