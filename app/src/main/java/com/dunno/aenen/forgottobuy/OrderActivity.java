e37a46f3dfbc3e3fb97dcd170d59d0c0966776b1
Yaroslav Golyuk <yaroslav.golyuk@elfo.net>
2020-02-24 11:05:55 +0200

    Codding challenge: phone dial; imeOptions="actionSend"

--- app/src/main/java/com/dunno/aenen/forgottobuy/OrderActivity.java
+++ app/src/main/java/com/dunno/aenen/forgottobuy/OrderActivity.java
@@ -2,15 +2,20 @@ package com.dunno.aenen.forgottobuy;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.amazon.euclid.widget.ZContainer;
@@ -19,6 +24,8 @@ import com.amazon.euclid.widget.ZTextView;
 
 
 public class OrderActivity extends Activity implements AdapterView.OnItemSelectedListener {
+
+    private static final String TAG = "ORDER_ACTIVITY_LOG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +42,7 @@ public class OrderActivity extends Activity implements AdapterView.OnItemSelecte
 
         // Create the spinner.
         Spinner spinner = (Spinner) findViewById(R.id.label_spinner);
-        if(spinner != null) {
+        if (spinner != null) {
             spinner.setOnItemSelectedListener(this);
         }
 
@@ -46,9 +53,23 @@ public class OrderActivity extends Activity implements AdapterView.OnItemSelecte
         // Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner.
-        if(spinner != null){
+        if (spinner != null) {
             spinner.setAdapter(adapter);
         }
+
+        EditText dialPhoneEditText = (EditText) findViewById(R.id.dial_phone_text);
+        if (dialPhoneEditText != null)
+            dialPhoneEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                    boolean handled = false;
+                    if (i == EditorInfo.IME_ACTION_SEND) {
+                        dialNumber();
+                        handled = true;
+                    }
+                    return handled;
+                }
+            });
     }
 
     /**
@@ -61,17 +82,17 @@ public class OrderActivity extends Activity implements AdapterView.OnItemSelecte
 
     public void onRadioButtonClicked(View view) {
         boolean checked = ((ZRadioButton) view).isChecked();
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.sameday:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.same_day_messenger_service));
                 break;
             case R.id.nextday:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.next_day_ground_delivery));
                 break;
             case R.id.pickup:
-                if(checked)
+                if (checked)
                     displayToast(getString(R.string.pick_up));
                 break;
             default:
@@ -81,7 +102,7 @@ public class OrderActivity extends Activity implements AdapterView.OnItemSelecte
         }
     }
 
-    public void displayToast(String message){
+    public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 
@@ -94,5 +115,27 @@ public class OrderActivity extends Activity implements AdapterView.OnItemSelecte
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
 
+    }
+
+    private void dialNumber() {
+
+        // Find the editText_main view.
+        EditText dialPhoneEditText = (EditText) findViewById(R.id.dial_phone_text);
+        String phoneNum = null;
+        // If the editText field is not null,
+        // concatenate "tel: " with the phone number string.
+        if (dialPhoneEditText != null) phoneNum = "tel:" + dialPhoneEditText.getText().toString();
+        // Optional: Log the concatenated phone number for dialing.
+        Log.d(TAG, "dialNumber: " + phoneNum);
+        // Specify the intent.
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        intent.setData(Uri.parse(phoneNum));
+        // If the intent resolves to a package (app),
+        // start the activity with the intent.
+        if (intent.resolveActivity(getPackageManager()) != null)
+            startActivity(intent);
+        else
+            Log.d("ImplicitIntents", "Can't handle this");
     }
 }
