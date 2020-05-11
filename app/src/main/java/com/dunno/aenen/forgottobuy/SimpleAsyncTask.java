55428efa087f41d01d686fa96eccc8b4967cd3a1
aenen <yaroslav.golyuk@gmail.com>
2020-05-11 22:12:49 +0300

    Android fundamentals 07.1: AsyncTask | Tasks

---
+++ app/src/main/java/com/dunno/aenen/forgottobuy/SimpleAsyncTask.java
@@ -0,0 +1,38 @@
+package com.dunno.aenen.forgottobuy;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
+import java.util.Random;
+
+/**
+ * Created by Yaroslav on 10.05.2020.
+ */
+public class SimpleAsyncTask extends AsyncTask<Void, Void, String>{
+    private WeakReference<TextView> mTextView;
+
+    SimpleAsyncTask(TextView tv) {
+        mTextView = new WeakReference<TextView>(tv);
+    }
+
+    @Override
+    protected String doInBackground(Void... voids) {
+        Random r = new Random();
+        int n = r.nextInt(11);
+
+        int s = n * 200;
+
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return "Awake at last after sleeping for " + s + " milliseconds!";
+    }
+
+    protected void onPostExecute(String result) {
+        mTextView.get().setText(result);
+    }
+}
