6965517ac45220c237f98c539fc18ff5538046a1
aenen <yaroslav.golyuk@gmail.com>
2020-05-29 01:08:38 +0300

    Android fundamentals 09.1: Shared preferences | Task

---
+++ app/src/main/java/com/dunno/aenen/forgottobuy/HelloSharedPrefsActivity.java
@@ -0,0 +1,116 @@
+package com.dunno.aenen.forgottobuy;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class HelloSharedPrefsActivity extends Activity {
+
+    // Current count
+    private int mCount = 0;
+    // Current background color
+    private int mColor;
+    // Text view to display both count and color
+    private TextView mShowCountTextView;
+
+    // Key for current count
+    private final String COUNT_KEY = "count";
+    // Key for current color
+    private final String COLOR_KEY = "color";
+
+    private SharedPreferences mPreferences;
+    private String sharedPrefFile = BuildConfig.APPLICATION_ID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_shared_prefs);
+
+        // Initialize views, color
+        mShowCountTextView = (TextView) findViewById(R.id.count_textview);
+        mColor = ContextCompat.getColor(this, R.color.default_background);
+        mPreferences = (SharedPreferences) getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
+        mCount = mPreferences.getInt(COUNT_KEY, 0);
+        mShowCountTextView.setText(String.format("%s", mCount));
+        mColor = mPreferences.getInt(COLOR_KEY, mColor);
+        mShowCountTextView.setBackgroundColor(mColor);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.putInt(COUNT_KEY, mCount);
+        preferencesEditor.putInt(COLOR_KEY, mColor);
+        preferencesEditor.apply();
+    }
+
+    /**
+     * Handles the onClick for the background color buttons. Gets background
+     * color of the button that was clicked, and sets the TextView background
+     * to that color.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void changeBackground(View view) {
+        int color = ((ColorDrawable) view.getBackground()).getColor();
+        mShowCountTextView.setBackgroundColor(color);
+        mColor = color;
+    }
+
+    /**
+     * Handles the onClick for the Count button. Increments the value of the
+     * mCount global and updates the TextView.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void countUp(View view) {
+        mCount++;
+        mShowCountTextView.setText(String.format("%s", mCount));
+    }
+
+    /**
+     * Saves the instance state if the activity is restarted (for example,
+     * on device rotation.) Here you save the values for the count and the
+     * background color.
+     *
+     * @param outState The state data.
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(COUNT_KEY, mCount);
+        outState.putInt(COLOR_KEY, mColor);
+    }
+
+    /**
+     * Handles the onClick for the Reset button. Resets the global count and
+     * background variables to the defaults and resets the views to those
+     * default values.
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void reset(View view) {
+        // Reset count
+        mCount = 0;
+        mShowCountTextView.setText(String.format("%s", mCount));
+
+        // Reset color
+        mColor = ContextCompat.getColor(this,
+                R.color.default_background);
+        mShowCountTextView.setBackgroundColor(mColor);
+
+        SharedPreferences.Editor preferencesEditor = mPreferences.edit();
+        preferencesEditor.clear();
+        preferencesEditor.apply();
+    }
+}
